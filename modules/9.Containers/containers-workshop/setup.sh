d5cca4b2f99fc7b7202f27dabf3353b6e0094f70
Alvaro Vera <alvaro_vera@epam.com>
2023-09-05 23:39:21 -0500

    Adding Killercoda configuration to containers

--- modules/9.Containers/containers-workshop/setup.sh
+++ modules/9.Containers/containers-workshop/setup.sh
@@ -3,7 +3,8 @@ set -x
 echo starting...
 
 git clone https://github.com/rolling-scopes-school/devops.git
-cd devops/modules/9.Containers/app/
+cd devops
 git checkout feature/containers
+cd modules/9.Containers/app/
 
 echo done > /tmp/finished
